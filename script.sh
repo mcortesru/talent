904dbc495b80b64605fa940b82f9acf77ab2a78f
mcortesru <>
2023-02-20 19:45:17 +0100

    script.sh guarda el json proporcionado por summa, pero además también guarda un json con los ids de las carpetas

--- script.sh
+++ script.sh
@@ -4,4 +4,6 @@
 curl --cookie-jar /tmp/cookies.txt "https://summa.upsa.es/j_security_check?j_username=talent&j_password=talent_2023"
 
 # Ejecutar el segundo comando y usar la cookie guardada en /tmp/cookies.txt
-curl --cookie /tmp/cookies.txt "https://summa.upsa.es/json/select.vm?query=acceptsComments:yes" > ./jsonCarpetas.json
+curl --cookie /tmp/cookies.txt "https://summa.upsa.es/json/select.vm?query=acceptsComments:yes" > ./carpetas.json
+
+cat carpetas.json | grep -o '"id":[^,}]*' | cut -d':' -f2 | sed 's/^\|$/"/g' | jq -n '{"ids": [inputs]}' | tr -d '\n' | tr -d ' ' > ./idsCarpetas.json
